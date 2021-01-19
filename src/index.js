b4cd850fc4d0e39404af05a56b6ccf0497e26ee3
David Johnston <djohnston@whispir.com>
2021-01-19 12:27:01 +1100

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const nodeSass = require("node-sass"); 
+const cors = require("cors")
 
 const express = require("express"); 
 
 const app = express(); 
 app.use(express.json());
+app.use(cors())
+
 
 const port = process.env.PORT || 3001; 
 
